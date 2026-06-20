abf59ac7717260d1f0e3a75c0020abf749b42b1a
Ninad N. Deodhare <2025.ninadd@isu.ac.in>
2026-06-20 15:58:27 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,6 @@ import React, { useState } from "react";
 
 const Navbar = () => {
 
-  React.useEffect(() => {
-    const id = setInterval(
-      () => setTime(new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", second: "2-digit" })),
-      1000
-    );
-    return () => clearInterval(id);
-  }, []);
-
   return (
     <div className="navbar">
       <div className="brand">
